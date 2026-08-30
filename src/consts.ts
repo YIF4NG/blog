6c864fb7aa5e7e4473356d4b23da3d4a3071c573
YIF4NG <yif4ng718@gmail.com>
2026-08-30 23:13:51 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -11,6 +11,6 @@ export const GITHUB_CONFIG = {
 	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'YIF4NG',
 	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || 'blog',
 	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
-	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '-',
+	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '4771336',
 	ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || 'wudishiduomejimo',
 } as const
